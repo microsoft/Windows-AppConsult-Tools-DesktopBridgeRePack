043a44af69667f1f9776698bfc0340be0a6d2f9f
Sebastien Bovo <sebastienbovo@outlook.com>
2017-10-18 22:57:36 +0200

    ok. Inside Appx folders have to be deleted

--- RepackageForWindows10S/MakeAPPXForWin10S.ps1
+++ RepackageForWindows10S/MakeAPPXForWin10S.ps1
@@ -79,6 +79,103 @@ function ModifyManifestFile ($ManifestFile) {
         exit
     }
 }
+
+
+function Work($AppxOrBundleFile, $InsideAppx) {
+    $FileExtension = ([System.IO.Path]::GetExtension($AppxOrBundleFile)).ToUpper()
+    # 1. Creates a new unique folder for extracting the Appx/Bundle files
+    $Index += 1
+    Write-Progress -Activity "[$($Index)/$($Steps)] Make Appx/Bundle for Windows 10S" -status "Extracting Appx/Bundle files" -PercentComplete ($Index / $Steps * 100)
+    $AppxPathOnly = Split-Path -Path $AppxOrBundleFile
+    if ($AppxPathOnly -eq "") # AppxOrBundleFile is located in the current directory
+    {
+        # AppxPathOnly = current path
+        $AppxPathOnly=Split-Path $MyInvocation.MyCommand.Path
+    }
+    # Does not use an unique folder name. Reusing the same folder in order to allow manual modifications
+    #$CurrentDateTime = Get-Date -UFormat "%Y-%m-%d-%Hh-%Mm-%Ss"
+    $AppxOrBundleFilenameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($AppxOrBundleFile)
+    $UnzippedFolder =  $AppxPathOnly + "\" + $AppxOrBundleFilenameWithoutExtension #+ "_" + $CurrentDateTime
+    Write-Host "[INFO] Unzipped folder = '$UnzippedFolder'"
+    Write-Host "[WORK] Extracting files from '$AppxOrBundleFile' to '$UnzippedFolder'..."
+
+    if($FileExtension -eq '.APPX') {
+        # APPX
+        & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe' unpack /l /p $AppxOrBundleFile /d $UnzippedFolder
+    }
+    else {
+        #BUNDLE
+        & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe' unbundle /p $AppxOrBundleFile /d $UnzippedFolder    
+    }
+    Write-Host "Done" -ForegroundColor Yellow
+    # =============================================================================
+
+
+    # 2. Modifies the 'CN' in the extracted AppxManifest.xml
+    $Index += 1
+    Write-Progress -Activity "[$($Index)/$($Steps)] Make Appx/Bundle for Windows 10S" -status "Modifying AppxManifest.xml file" -PercentComplete ($Index / $Steps * 100)
+
+    # So we are looking for Publisher="CN=Blabla.�&  blablabla!?; etc..."
+    # or Publisher='CN=Blabla.�&  blablabla!?; etc...'
+    if($FileExtension -eq '.APPX') {
+        # APPX
+        $AppxManifestFile = $UnzippedFolder + "\AppxManifest.xml"
+        Write-Host "[WORK] Modifying the '$AppxManifestFile' to use Publisher=""CN=Appx Test Root Agency Ex""..."
+        ModifyManifestFile($AppxManifestFile)    
+    }
+    else {
+        # BUNDLE
+        $AppxManifestFile = $UnzippedFolder + "\AppxMetadata\AppxBundleManifest.xml"
+        Write-Host "[WORK] Modifying the '$AppxManifestFile' to use Publisher=""CN=Appx Test Root Agency Ex""..."
+        ModifyManifestFile($AppxManifestFile)
+        
+        # All Manifest of all packages have to be modified
+        Get-ChildItem $UnzippedFolder -Filter *.appx | 
+        Foreach-Object {
+            Work -AppxOrBundleFile $_.FullName -InsideAppx $true     
+        }
+    }
+
+    # =============================================================================
+
+
+    # 3. Recreates the Appx/Bundle file with the modified AppxManifest.xml
+    $Index += 1
+    Write-Progress -Activity "[$($Index)/$($Steps)] Make Appx/Bundle for Windows 10S" -status "Repackaging the Appx/Bundle file" -PercentComplete ($Index / $Steps * 100)
+    $ModifiedAppxBundleFile = ""
+    if($FileExtension -eq '.APPX') {
+        # APPX
+        if ($InsideAppx) {
+            $ModifiedAppxBundleFile = $AppxOrBundleFile
+        }
+        else {
+            $ModifiedAppxBundleFile = $AppxPathOnly + "\" + $AppxOrBundleFilenameWithoutExtension + "StoreSigned.appx"
+        }
+        & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe' pack -p $ModifiedAppxBundleFile -d $UnzippedFolder
+    }
+    else {
+        #BUNDLE
+        $ModifiedAppxBundleFile = $AppxPathOnly + "\" + $AppxOrBundleFilenameWithoutExtension + "StoreSigned.appxbundle"
+        & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe' bundle -p $ModifiedAppxBundleFile -d $UnzippedFolder
+    }
+    Write-Host "Done" -ForegroundColor Yellow
+    # =============================================================================
+
+
+    # 4. Sign the Appx/Bundle file with the AppxTestRootAgency providedby the Store team
+    $Index += 1
+    Write-Progress -Activity "[$($Index)/$($Steps)] Make Appx/Bundle for Windows 10S" -status "Signing the Appx file" -PercentComplete ($Index / $Steps * 100)
+    & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe' sign /a /v /fd SHA256 /f "AppxTestRootAgency.pfx" $ModifiedAppxBundleFile
+    Write-Host "Done" -ForegroundColor Yellow
+    # =============================================================================
+
+
+    Write-Host "`nNewly and signed Appx/Bundle file available at " -nonewline
+    Write-Host "$ModifiedAppxBundleFile" -ForegroundColor Green
+
+    # App packager (MakeAppx.exe) - https://msdn.microsoft.com/en-us/library/windows/desktop/hh446767(v=vs.85).aspx
+    # Porting and testing your classic desktop applications on Windows 10 S with the Desktop Bridge - https://blogs.msdn.microsoft.com/appconsult/2017/06/15/porting-and-testing-your-classic-desktop-applications-on-windows-10-s-with-the-desktop-bridge/
+}
 # =============================================================================
 
 
@@ -108,92 +205,4 @@ Write-Host "[INFO] AppxOrBundle = '$AppxOrBundleFile'"
 $Index = 0
 $Steps = 4
 
-# 1. Creates a new unique folder for extracting the Appx/Bundle files
-$Index += 1
-Write-Progress -Activity "[$($Index)/$($Steps)] Make Appx/Bundle for Windows 10S" -status "Extracting Appx/Bundle files" -PercentComplete ($Index / $Steps * 100)
-$AppxPathOnly = Split-Path -Path $AppxOrBundleFile
-if ($AppxPathOnly -eq "") # AppxOrBundleFile is located in the current directory
-{
-    # AppxPathOnly = current path
-    $AppxPathOnly=Split-Path $MyInvocation.MyCommand.Path
-}
-# Does not use an unique folder name. Reusing the same folder in order to allow manual modifications
-#$CurrentDateTime = Get-Date -UFormat "%Y-%m-%d-%Hh-%Mm-%Ss"
-$AppxOrBundleFilenameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($AppxOrBundleFile)
-$UnzippedFolder =  $AppxPathOnly + "\" + $AppxOrBundleFilenameWithoutExtension #+ "_" + $CurrentDateTime
-Write-Host "[INFO] Unzipped folder = '$UnzippedFolder'"
-Write-Host "[WORK] Extracting files from '$AppxOrBundleFile' to '$UnzippedFolder'..."
-
-if($FileExtension -eq '.APPX') {
-    # APPX
-    & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe' unpack /l /p $AppxOrBundleFile /d $UnzippedFolder
-}
-else {
-    #BUNDLE
-    & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe' unbundle /p $AppxOrBundleFile /d $UnzippedFolder    
-}
-Write-Host "Done" -ForegroundColor Yellow
-# =============================================================================
-
-
-# 2. Modifies the 'CN' in the extracted AppxManifest.xml
-$Index += 1
-Write-Progress -Activity "[$($Index)/$($Steps)] Make Appx/Bundle for Windows 10S" -status "Modifying AppxManifest.xml file" -PercentComplete ($Index / $Steps * 100)
-
-# So we are looking for Publisher="CN=Blabla.�&  blablabla!?; etc..."
-# or Publisher='CN=Blabla.�&  blablabla!?; etc...'
-if($FileExtension -eq '.APPX') {
-    # APPX
-    $AppxManifestFile = $UnzippedFolder + "\AppxManifest.xml"
-    Write-Host "[WORK] Modifying the '$AppxManifestFile' to use Publisher=""CN=Appx Test Root Agency Ex""..."
-    ModifyManifestFile($AppxManifestFile)    
-}
-else {
-    # BUNDLE
-    $AppxManifestFile = $UnzippedFolder + "\AppxMetadata\AppxBundleManifest.xml"
-    Write-Host "[WORK] Modifying the '$AppxManifestFile' to use Publisher=""CN=Appx Test Root Agency Ex""..."
-    ModifyManifestFile($AppxManifestFile)
-    
-    # All Manifest of all packages have to be modified
-    Get-ChildItem $UnzippedFolder -Filter *.appx | 
-    Foreach-Object {
-        & ((Split-Path $MyInvocation.InvocationName) + "\MakeAPPXForWin10S.cmd") $_.FullName        
-    }
-}
-
-# =============================================================================
-
-
-# 3. Recreates the Appx/Bundle file with the modified AppxManifest.xml
-$Index += 1
-Write-Progress -Activity "[$($Index)/$($Steps)] Make Appx/Bundle for Windows 10S" -status "Repackaging the Appx/Bundle file" -PercentComplete ($Index / $Steps * 100)
-$ModifiedAppxBundleFile = ""
-if($FileExtension -eq '.APPX') {
-    # APPX
-    $ModifiedAppxBundleFile = $AppxPathOnly + "\" + $AppxOrBundleFilenameWithoutExtension + "StoreSigned.appx"
-    & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe' pack -p $ModifiedAppxBundleFile -d $UnzippedFolder
-}
-else {
-    #BUNDLE
-    $ModifiedAppxBundleFile = $AppxPathOnly + "\" + $AppxOrBundleFilenameWithoutExtension + "StoreSigned.appxbundle"
-    & 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe' bundle -p $ModifiedAppxBundleFile -d $UnzippedFolder
-}
-Write-Host "Done" -ForegroundColor Yellow
-# =============================================================================
-
-
-# 4. Sign the Appx/Bundle file with the AppxTestRootAgency providedby the Store team
-$Index += 1
-Write-Progress -Activity "[$($Index)/$($Steps)] Make Appx/Bundle for Windows 10S" -status "Signing the Appx file" -PercentComplete ($Index / $Steps * 100)
-& 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe' sign /a /v /fd SHA256 /f "AppxTestRootAgency.pfx" $ModifiedAppxBundleFile
-Write-Host "Done" -ForegroundColor Yellow
-# =============================================================================
-
-
-Write-Host "`nNewly and signed Appx/Bundle file available at " -nonewline
-Write-Host "$ModifiedAppxBundleFile" -ForegroundColor Green
-
-# App packager (MakeAppx.exe) - https://msdn.microsoft.com/en-us/library/windows/desktop/hh446767(v=vs.85).aspx
-# Porting and testing your classic desktop applications on Windows 10 S with the Desktop Bridge - https://blogs.msdn.microsoft.com/appconsult/2017/06/15/porting-and-testing-your-classic-desktop-applications-on-windows-10-s-with-the-desktop-bridge/
-
-
+Work -AppxOrBundleFile $AppxOrBundleFile -InsideAppx $false  
